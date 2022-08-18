559a16ae4770a07cb963139253dae2914c64a2d3
Tristan Daniël Maat <tm@tlater.net>
2022-08-18 15:15:22 +0100

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "Simple Minecraft datapack to remove elytras from the game";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-22.05";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    packages.${system}.default =
+      pkgs.stdenv.mkDerivation {
+        pname = "elytrant";
+        version = "1.0";
+
+        src = pkgs.lib.sources.cleanSource self;
+
+        noCC = true;
+        buildInputs = with pkgs; [ zip ];
+
+        buildPhase = ''
+          zip -r elytrant.zip .
+        '';
+
+        installPhase = ''
+          mkdir -p $out
+          cp elytrant.zip $out/
+        '';
+      };
+  };
+}
